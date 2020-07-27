02c1be899ac09589758e03ff909fe8dfda68107f
Jesse O'Donoghue <jesseodonoghue@gmail.com>
2020-07-26 23:26:09 -0400

    Completed coding on index.html, script.js, style.css

--- script.js
+++ script.js
@@ -6,35 +6,44 @@ $(document).ready(function() {
     var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
-    console.log('value:', value);
-    console.log('time:', time);
+    // console.log('value:', value);
+    // console.log('time:', time);
 
     // save the value in localStorage as time
+    localStorage.setItem(time, value);
     
   });
 
   function hourUpdater() {
     // get current number of hours
     var currentHour = moment().hours();
-    console.log('current hour:', currentHour);
+    // console.log('current hour:', currentHour);
 
     // loop over time blocks
     $(".time-block").each(function() {
       var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
-      console.log("block hour:", blockHour);
+      // console.log("block hour:", blockHour);
 
       // check if we've moved past this time
+      if (blockHour < currentHour) {        
       
       // if the current hour is greater than the block hour
       // then add class "past"
+        $(this).addClass("past");
 
       // if they are equal
       // then remove class "past" and add class "present"
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past").addClass("present");
 
       // else
       // remove class "past", remove class "present", add class "future"
-      
+      } else {
+        $(this).removeClass("past").removeClass("present").addClass("future");
+
+      }
+
     });
   }
 
@@ -42,8 +51,17 @@ $(document).ready(function() {
 
   // set up interval to check if current time needs to be updated
   // which means execute hourUpdater function every 15 seconds
+  setInterval(() => {
+    
+    hourUpdater();
+
+  }, 15000);
 
   // load any saved data from localStorage
+  $('.time-block').each(function() {
+    var time = $(this).attr("id");
+    $(this).children(".description").val(localStorage.getItem(time));
+  });
   
 
   // display current day on page
